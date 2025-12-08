b29aab18da55e99b0ce11a38841781889b253927
kvr-ui <kvr@focasedu.com>
2025-12-08 10:23:21 +0000

    cors error

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://127.0.0.1:5174',
     'http://localhost:5174',
+    'https://kit.focasedu.com',
     'http://127.0.0.1:5173'
   ],
   credentials: true
